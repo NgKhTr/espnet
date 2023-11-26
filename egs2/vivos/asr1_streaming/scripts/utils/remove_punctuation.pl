168c9ec7162853b142d5e8a95c7ea996d07376c9
NgKhTr <ktrinh1032003@gmail.com>
2023-11-26 17:22:21 +0700

    fix

--- egs2/vivos/asr1_streaming/scripts/utils/remove_punctuation.pl
+++ egs2/vivos/asr1_streaming/scripts/utils/remove_punctuation.pl
@@ -1,1 +1,25 @@
-../../../../../utils/remove_punctuation.pl
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+binmode(STDIN,":utf8");
+binmode(STDOUT,":utf8");
+
+while(<STDIN>) {
+  $_ = " $_ ";
+
+  # remove punctuation except apostrophe
+  s/<space>/spacemark/g;  # for scoring
+  s/'/apostrophe/g;
+  s/[[:punct:]]//g;
+  s/apostrophe/'/g;
+  s/spacemark/<space>/g;  # for scoring
+
+  # remove whitespace
+  s/\s+/ /g;
+  s/^\s+//;
+  s/\s+$//;
+
+  print "$_\n";
+}
