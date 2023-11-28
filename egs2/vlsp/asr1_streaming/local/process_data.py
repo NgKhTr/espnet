4800aa95e9264714aa631325ecc2f211a162b67c
NgKhTr <ktrinh1032003@gmail.com>
2023-11-28 20:28:43 +0700

    process_data.py

--- egs2/vlsp/asr1_streaming/local/process_data.py
+++ egs2/vlsp/asr1_streaming/local/process_data.py
@@ -11,10 +11,12 @@ from sklearn.model_selection import train_test_split
 train_utt_id, val_test_utt_id = train_test_split(list_utt_id, train_size=0.8, random_state = RANDOM_STATE)
 val_utt_id, test_utt_id = train_test_split(val_test_utt_id, train_size=0.5, random_state = RANDOM_STATE)
 
-def process_data(folder_contain, list_utt_id):
+def process_data(folder_contain, list_utt_id: list):
     # wav.scp: Mappinng a utterance-ID to a path of audio file
+    list_utt_id.sort() # sort r ở đây r nè
     with open(f"data/{folder_contain}/wav.scp", 'w') as file:
-        file.writelines([f'{utt_id} {VLSP_PATH}/{utt_id}.wav\n' for utt_id in list_utt_id])
+        lst = list(map(lambda utt_id: f'{utt_id} {VLSP_PATH}/{utt_id}.wav\n', list_utt_id))
+        file.writelines(lst)
 
     # text: Mapping a utterance-ID to a text
     def read_utt(utt_id):
@@ -23,7 +25,8 @@ def process_data(folder_contain, list_utt_id):
             res = text.read()
         return res
     with open(f'data/{folder_contain}/text', 'w') as file:
-        file.writelines([f'{utt_id} {read_utt(utt_id)}\n' for utt_id in list_utt_id])
+        lst = list(map(lambda utt_id: f'{utt_id} {read_utt(utt_id)}\n', list_utt_id))
+        file.writelines(lst)
 
     # spk2utt: Mapping a speaker-ID to a list of utter/ance-IDs
         # do chỉ có 1 th speaker
@@ -34,16 +37,16 @@ def process_data(folder_contain, list_utt_id):
 
     # utt2spk: Mappinng a utterance-ID to a speaker-ID
     with open(f"data/{folder_contain}/utt2spk", 'w') as file:
-        file.writelines([f'{utt_id} {speaker_name}\n' for utt_id in list_utt_id])
-
+        lst = list(map(lambda utt_id: f'{utt_id} {speaker_name}\n', list_utt_id))
+        file.writelines(lst)
 
 process_data('train', train_utt_id)
 process_data('test', test_utt_id)
 process_data('valid', val_utt_id)
 
-with open('data/train/wav.scp', 'r') as file:
-    print('Train sample amount:', len(file.readlines()))
-with open('data/test/wav.scp', 'r') as file:
-    print('Test sample amount:', len(file.readlines()))
-with open('data/valid/wav.scp', 'r') as file:
-    print('Valid sample amount:', len(file.readlines()))
+# with open('data/train/wav.scp', 'r') as file:
+#     print('Train sample amount:', len(file.readlines()))
+# with open('data/test/wav.scp', 'r') as file:
+#     print('Test sample amount:', len(file.readlines()))
+# with open('data/valid/wav.scp', 'r') as file:
+#     print('Valid sample amount:', len(file.readlines()))
